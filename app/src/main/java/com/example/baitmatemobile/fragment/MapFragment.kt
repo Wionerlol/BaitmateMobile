0d1c50fc4755cf333be4e749d95e127a70ae6c5e
hannah <hannah.wong@u.nus.edu>
2025-01-31 14:36:49 +0800

    add weather forecasts using data.gov 2h forecast weather API

--- app/src/main/java/com/example/baitmatemobile/fragment/MapFragment.kt
+++ app/src/main/java/com/example/baitmatemobile/fragment/MapFragment.kt
@@ -11,6 +11,7 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.baitmatemobile.R
 
@@ -22,6 +23,12 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import org.json.JSONArray
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Locale
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MapFragment : Fragment() {
 
@@ -84,6 +91,7 @@ class MapFragment : Fragment() {
 
                     val position = LatLng(latitude, longitude)
                     googleMap.addMarker(MarkerOptions().position(position).title(name))
+                    fetchWeatherForecast(name, position)
                 } catch (e: Exception) {
                     Log.e("MapFragment", "Error parsing location at index $i: ${e.message}")
                 }
@@ -91,5 +99,71 @@ class MapFragment : Fragment() {
         } else {
             Log.d("MapFragment", "No locations found in response")
         }
+
+        googleMap.setOnMarkerClickListener { marker ->
+            marker.showInfoWindow()
+            true
+        }
     }
+
+    private fun fetchWeatherForecast(locationName: String, position: LatLng) {
+        val currentTime = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault()).format(Date())
+        val url = "https://api-open.data.gov.sg/v2/real-time/api/two-hr-forecast?date=$currentTime"
+
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                Log.d("MapFragment", "Weather response received.")
+                val forecast = parseWeatherForecast(response, position)
+                val validPeriod = parseValidPeriod(response)
+                updateMarkerWithWeather(position, locationName, forecast, validPeriod)
+            },
+            { error ->
+                Log.e("MapFragment", "Weather request error: ${error.message}")
+                error.printStackTrace()
+            }
+        )
+        requestQueue.add(request)
+    }
+
+    private fun parseWeatherForecast(response: JSONObject, position: LatLng): String {
+        val areaMetaData = response.getJSONObject("data").getJSONArray("area_metadata")
+        val forecasts = response.getJSONObject("data").getJSONArray("items").getJSONObject(0).getJSONArray("forecasts")
+        var nearestForecast = "No forecast available"
+        var minDistance = Double.MAX_VALUE
+
+        for (i in 0 until areaMetaData.length()) {
+            val area = areaMetaData.getJSONObject(i)
+            val labelLocation = area.getJSONObject("label_location")
+            val areaLatLng = LatLng(labelLocation.getDouble("latitude"), labelLocation.getDouble("longitude"))
+            val distance = calculateDistance(position, areaLatLng)
+            if (distance < minDistance) {
+                minDistance = distance
+                nearestForecast = forecasts.getJSONObject(i).getString("forecast")
+            }
+        }
+        return nearestForecast
+
+    }
+
+    private fun calculateDistance(from: LatLng, to: LatLng): Double {
+        val latDiff = from.latitude - to.latitude
+        val lonDiff = from.longitude - to.longitude
+        return sqrt(latDiff.pow(2) + lonDiff.pow(2))
+    }
+
+    private fun parseValidPeriod(response: JSONObject): String {
+        val validPeriod = response.getJSONObject("data").getJSONArray("items").getJSONObject(0).getJSONObject("valid_period")
+        return validPeriod.getString("text")
+    }
+
+    private fun updateMarkerWithWeather(position: LatLng, locationName: String, forecast: String, validPeriod: String) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(position)
+                .title(locationName)
+                .snippet("2h forecast: $forecast\nValid: $validPeriod")
+        )
+    }
+
 }
