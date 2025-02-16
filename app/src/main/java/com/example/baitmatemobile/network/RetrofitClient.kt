3642da897df83d0cc6e991737c3ab3f20453f78c
CenHaoyang <maxcen@qq.com>
2025-02-16 16:25:38 +0800

    Change to remote link

--- app/src/main/java/com/example/baitmatemobile/network/RetrofitClient.kt
+++ app/src/main/java/com/example/baitmatemobile/network/RetrofitClient.kt
@@ -7,8 +7,8 @@ import okhttp3.OkHttpClient
 
 
 object RetrofitClient {
-    //private const val BASE_URL = "http://54.179.221.8:8080/api/"
-    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+    private const val BASE_URL = "http://54.179.221.8:8080/api/"
+    //private const val BASE_URL = "http://10.0.2.2:8080/api/"
 
     private val okHttpClient = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
